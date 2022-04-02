35b824e74a5563d27b683ce9267a0e8a9d983dbe
EMNayon <emranmasudnayon.017@gmail.com>
2022-04-03 02:07:31 +0600

    I learned for loop blade view

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -6,3 +6,8 @@
 
 {!! $XSSKey !!} 
 {{ $XSSKey }}
+<br>
+@for($i=0;$i<100; $i++)
+    <!-- <button>{{$i}}</button> -->
+    <img src="https://scontent.fdac116-1.fna.fbcdn.net/v/t39.30808-6/274927537_1869157803289763_1086383566687088094_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=e3f864&_nc_ohc=ljedv9oYXysAX8hdnig&tn=CLESIfFqz07Qvb4s&_nc_ht=scontent.fdac116-1.fna&oh=00_AT_tK5VXRtfmhggRT104WdOB-rCpmgwiBMMT_yD1MTmsaA&oe=624D002F" style="width:50px;height:30px">
+@endfor
