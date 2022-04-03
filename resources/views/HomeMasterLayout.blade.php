117c18b8455ff59a981430344480c2e249628e73
EMNayon <emranmasudnayon.017@gmail.com>
2022-04-03 17:04:37 +0600

    I learned , how to multiple content pass the parent class

--- resources/views/HomeMasterLayout.blade.php
+++ resources/views/HomeMasterLayout.blade.php
@@ -1,5 +1,14 @@
 @extends('layout.MasterLayout')
-
-
 @section('titleKey','Home Page')
-@section('buttonName','Submit Now')
+
+
+
+@section('content')
+<button class = "btn btn-primary">Sign up</button> <br><br>
+<button class = "btn btn-primary">Sign in</button> <br><br>
+<button class = "btn btn-primary">Log in</button> <br><br>
+<button class = "btn btn-primary">Log out</button> <br><br>
+<button class = "btn btn-primary">Sign up</button> <br><br>
+<button class = "btn btn-primary">Sign up</button> <br>
+@endsection
+
