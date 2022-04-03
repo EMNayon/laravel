255c0799ba29fc2896d6eab8741886bc6aa55534
EMNayon <emranmasudnayon.017@gmail.com>
2022-04-03 16:12:30 +0600

    some modify. and I learned includeifand includefirst function

--- resources/views/IncludingSubviewBladeView.blade.php
+++ resources/views/IncludingSubviewBladeView.blade.php
@@ -1,8 +1,8 @@
-@includeif('subview.Men',['DataKey'=> $DataKey])
+@includefirst(['subview.Footer','subview.Menu'])
 
-<br>
+<!-- <br>
 <br><br><br><br><br><br><br><br><br>
 <h1>This is my home page</h1>
 <br><br><br><br><br><br><br><br><br><br>
 
-@include('subview.Footer',['footerText'=>'All Rights Reserved By EM Nayon'])
+@include('subview.Footer',['footerText'=>'All Rights Reserved By EM Nayon']) -->
